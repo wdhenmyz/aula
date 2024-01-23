0d8811ef6d0fc9078042e8451901f60dcf6d739e
wdhenmyz <wesllen.dhenmyz@outlook.com>
2024-01-23 11:34:50 -0300

    push

--- casas de tabuleiro.js
+++ casas de tabuleiro.js
@@ -1,9 +1,9 @@
-var base = 18;
+var base = 2;
 var expoente = 0;
 var casastabuleiro = 64;
 var resultado = 0;
 
-for(base=18;expoente<=casastabuleiro;expoente++){
+for(base=2;expoente<=casastabuleiro;expoente++){
     resultado = base**expoente;
     console.log(resultado);
 }
