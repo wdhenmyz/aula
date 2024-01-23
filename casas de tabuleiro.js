ffab8d93a47d53acb20bbbca0cbf9bb03543035b
wdhenmyz <wesllen.dhenmyz@outlook.com>
2024-01-23 11:26:15 -0300

    changed

---
+++ casas de tabuleiro.js
@@ -0,0 +1,10 @@
+var base = 18;
+var expoente = 0;
+var casastabuleiro = 64;
+var resultado = 0;
+
+for(base=18;expoente<=casastabuleiro;expoente++){
+    resultado = base**expoente;
+    console.log(resultado);
+}
+
--- dobroDeArroz.js
+++
@@ -1,7 +0,0 @@
-var start = 1;
-var end = 64;
-var store = 1;
-
-for(start=1;start<=end;){
-
-}
