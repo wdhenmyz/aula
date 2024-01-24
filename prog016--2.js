0a9ee432b2195c57198cdc9e0ad15c720e5220e9
wdhenmyz <wesllen.dhenmyz@outlook.com>
2024-01-24 11:35:42 -0300

    push

---
+++ prog016--2.js
@@ -0,0 +1,5 @@
+var emptyArray = [];
+for (start = 20; start > 0; start--) {
+    emptyArray[start] = 1;
+    console.log(start);
+}
---
+++ prog016--3.js
@@ -0,0 +1,24 @@
+var emptyArray = [];
+
+emptyArray[0]= 'alan';
+
+emptyArray.push('alex');
+emptyArray.push('alisson');
+emptyArray.push('daniel');
+console.log(emptyArray);
+console.log('#########################');
+emptyArray.shift();
+console.log(emptyArray);
+
+
+emptyArray.pop();
+console.log(emptyArray);
+
+emptyArray.shift();
+console.log(emptyArray);
+console.log(emptyArray[0]);
+
+emptyArray.unshift('alan');
+console.log(emptyArray);
+
+console.log('########################');
