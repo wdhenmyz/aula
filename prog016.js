66ae4a5c742e2f6ca5a7c76d066ee6157c57142c
wdhenmyz <wesllen.dhenmyz@outlook.com>
2024-01-24 08:59:43 -0300

    push

--- prog016.js
+++ prog016.js
@@ -2,8 +2,8 @@
 var emptyArray = [];
 
 var fruits = ['apple', 'orange', 'banana'];
+//console.log(fruits);
 
 var ages = [45 , 85 , 78];
 
 var test = ['berg', 36, '2351654354', true];
-console.log(typeof test);
